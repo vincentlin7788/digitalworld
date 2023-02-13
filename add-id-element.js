6885c05b78cf6be9a1eef60303ed11320064920c
vincentlin7788 <1459034@qq.com>
2023-02-13 13:46:33 +0800

    sss

---
+++ add-id-element.js
@@ -0,0 +1,8 @@
+<script>
+var lelements = document.getElementsByTagName('h5');
+if (lelements != null &&  typeof(lelements) != "undefined" ) {
+for (var i=0; i<lelements.length; i++) {
+    lelements[i].setAttribute('id', 'clickListItm');
+}
+}
+</script>
